c53a4139e140c494df546d658e2d70b3efbeb129
Derek <dmaxin@uwaterloo.ca>
2024-07-12 03:10:19 -0400

    Updates

---
+++ precache-manifest.3c548b6d2641e71f42176d10ce847706.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9d23b4d9e76c840e2268a1c4ffcb92",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "68575795ebe80728e7e8",
+    "url": "/Portfolio-Website/static/css/2.3e6e03c3.chunk.css"
+  },
+  {
+    "revision": "bef0df3e97638f0ebdf8",
+    "url": "/Portfolio-Website/static/css/main.75c9a26f.chunk.css"
+  },
+  {
+    "revision": "68575795ebe80728e7e8",
+    "url": "/Portfolio-Website/static/js/2.25a6546d.chunk.js"
+  },
+  {
+    "revision": "cf46fa9c8064673a485fe95feff73812",
+    "url": "/Portfolio-Website/static/js/2.25a6546d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a9e9dd2d26fc3ceaf0",
+    "url": "/Portfolio-Website/static/js/3.6cd3267f.chunk.js"
+  },
+  {
+    "revision": "bef0df3e97638f0ebdf8",
+    "url": "/Portfolio-Website/static/js/main.859826b9.chunk.js"
+  },
+  {
+    "revision": "dd283f72b682bdd4a8f9",
+    "url": "/Portfolio-Website/static/js/runtime-main.bcd6c804.js"
+  },
+  {
+    "revision": "a7e580cb3bd89c20500fce34a3b63e56",
+    "url": "/Portfolio-Website/static/media/AWS.a7e580cb.png"
+  },
+  {
+    "revision": "441104121f45696d5db055686ef3f6f3",
+    "url": "/Portfolio-Website/static/media/Azure.44110412.png"
+  },
+  {
+    "revision": "da17fec3b5a83057cfcec9b9fb2935c2",
+    "url": "/Portfolio-Website/static/media/Bar.da17fec3.PNG"
+  },
+  {
+    "revision": "b4dd4c5e18b3c0a0c35018f37c2b2f94",
+    "url": "/Portfolio-Website/static/media/C.b4dd4c5e.png"
+  },
+  {
+    "revision": "0b849c72f38362fe12072a4916660013",
+    "url": "/Portfolio-Website/static/media/Cplusplus.0b849c72.png"
+  },
+  {
+    "revision": "88a515c6a91f5e3ac8eb5654eb4d77e0",
+    "url": "/Portfolio-Website/static/media/End.88a515c6.png"
+  },
+  {
+    "revision": "a0460b95a432081ebb4979800f2cc53e",
+    "url": "/Portfolio-Website/static/media/Firebase.a0460b95.png"
+  },
+  {
+    "revision": "450c485227b508a918494e1d97980ff7",
+    "url": "/Portfolio-Website/static/media/Game.450c4852.PNG"
+  },
+  {
+    "revision": "ca1ed85aaefc4f251082645b111057bc",
+    "url": "/Portfolio-Website/static/media/Git.ca1ed85a.png"
+  },
+  {
+    "revision": "4b16f4768b4a8b409f0f7118b37c163e",
+    "url": "/Portfolio-Website/static/media/Grid.4b16f476.PNG"
+  },
+  {
+    "revision": "40ac4f2c6aa6d93c29c8d0be0f2a14a3",
+    "url": "/Portfolio-Website/static/media/Home.40ac4f2c.jpg"
+  },
+  {
+    "revision": "200da24bccfffc58c8cd2607b1931c14",
+    "url": "/Portfolio-Website/static/media/Intermediate.200da24b.png"
+  },
+  {
+    "revision": "b36f1131a6e2ae39247217311cdcaf0a",
+    "url": "/Portfolio-Website/static/media/JS.b36f1131.png"
+  },
+  {
+    "revision": "908bcbe9a94d2bc69089a01a356d3a24",
+    "url": "/Portfolio-Website/static/media/Java.908bcbe9.png"
+  },
+  {
+    "revision": "5bb0d08faee23a086d848c7b412cc840",
+    "url": "/Portfolio-Website/static/media/Jenkins.5bb0d08f.png"
+  },
+  {
+    "revision": "a9aa28c93ceed581953a01df646170a8",
+    "url": "/Portfolio-Website/static/media/Kotlin.a9aa28c9.png"
+  },
+  {
+    "revision": "87c3f2b68eec7c58e6386719d1fc4354",
+    "url": "/Portfolio-Website/static/media/Kubernetes.87c3f2b6.png"
+  },
+  {
+    "revision": "c58776a775f7224ac13933c71f348fb6",
+    "url": "/Portfolio-Website/static/media/Line.c58776a7.PNG"
+  },
+  {
+    "revision": "ee879c165d2e804dbe73ea7ec233c5b0",
+    "url": "/Portfolio-Website/static/media/List.ee879c16.PNG"
+  },
+  {
+    "revision": "c43eec5d01a7bdbd9b0df0cd38ce3780",
+    "url": "/Portfolio-Website/static/media/Login.c43eec5d.jpg"
+  },
+  {
+    "revision": "e0359c7c9932302fd91afc10741b465b",
+    "url": "/Portfolio-Website/static/media/Login.e0359c7c.png"
+  },
+  {
+    "revision": "54c9cdce7368e68c8f8a6eede4812048",
+    "url": "/Portfolio-Website/static/media/Lua.54c9cdce.png"
+  },
+  {
+    "revision": "5b854620c32270205b96ba6f7000d14a",
+    "url": "/Portfolio-Website/static/media/Pie.5b854620.PNG"
+  },
+  {
+    "revision": "f67907ceb8786f5360e16e5d885082d3",
+    "url": "/Portfolio-Website/static/media/Profile unexpanded.f67907ce.jpg"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/Portfolio-Website/static/media/React.37fe8322.png"
+  },
+  {
+    "revision": "e93998ca8f6b3fb374225edb92ffa6cb",
+    "url": "/Portfolio-Website/static/media/Register.e93998ca.jpg"
+  },
+  {
+    "revision": "29b524d0b6f487be03dea49ee2506120",
+    "url": "/Portfolio-Website/static/media/SQL.29b524d0.png"
+  },
+  {
+    "revision": "36a758d62f53f1814053e27f09836d17",
+    "url": "/Portfolio-Website/static/media/Search and filter.36a758d6.jpg"
+  },
+  {
+    "revision": "ecfeeae8d334868f9f962d051490c400",
+    "url": "/Portfolio-Website/static/media/VSCode.ecfeeae8.png"
+  },
+  {
+    "revision": "84760d6405bd7e6109aab230735c6405",
+    "url": "/Portfolio-Website/static/media/bark texture.84760d64.png"
+  },
+  {
+    "revision": "a1b00e5c2c9dd6c895d45c990e9b191a",
+    "url": "/Portfolio-Website/static/media/cloud1.a1b00e5c.png"
+  },
+  {
+    "revision": "2d78c0b33dd15efbaafb0d4fa21311dc",
+    "url": "/Portfolio-Website/static/media/cloud2.2d78c0b3.png"
+  },
+  {
+    "revision": "e2f627776c8fc0dc9b6f7f806147f20f",
+    "url": "/Portfolio-Website/static/media/cloud4.e2f62777.png"
+  },
+  {
+    "revision": "5ef7bbd1bfeff400feb2df2661781993",
+    "url": "/Portfolio-Website/static/media/mount douglas.5ef7bbd1.jpg"
+  },
+  {
+    "revision": "6ef875491918d171a01533da193ab2a6",
+    "url": "/Portfolio-Website/static/media/myself.6ef87549.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Portfolio-Website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Portfolio-Website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Portfolio-Website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Portfolio-Website/static/media/slick.f97e3bbf.svg"
+  }
+]);
